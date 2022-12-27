2f0dfd991d21dad156f99e816099d8d7a439cb19
tommyshelby1917 <tommyshelby@disroot.org>
2022-12-27 22:57:12 +0100

    Script added into index.html and custom hook for tracking navigation and events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import { usePageTracking } from './hooks/usePageTracking';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    usePageTracking();
+
+    return (
+        <Routes>
+            <Route path="/" element={<h1>Route One</h1>} />
+            <Route path="/two" element={<h1>Route Two</h1>} />
+            <Route path="/two" element={<h1>Route Three</h1>} />
+        </Routes>
+    );
 }
 
 export default App;
